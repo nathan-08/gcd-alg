d5ba5cdaf320600f13c5079f8507d145850ac486
Nathan Klundt <nathan@blockchainjedi.com>
2022-08-26 11:18:02 -0600

    fix egcd

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@
 // for finding the greatest common divisor (divisor communis maximus)
 
 fn gcd(a: i32, b: i32) -> i32 {
-    let r = a % b;
-
-    match r {
+    match a % b {
         0 => b,
-        _ => gcd(b, r)
+        r => gcd(b, r)
     }
 }
 
@@ -17,12 +15,13 @@ fn egcd(a: i32, b:i32) -> (i32, i32) {
     let mut q = x2 * a + y2 * b;
 
     while p % q != 0 {
-        let x1_ = x1;
-        let y1_ = y1;
+        let n = p / q;
+        let x1_prev = x1;
+        let y1_prev = y1;
         x1 = x2;
         y1 = y2;
-        x2 = x1_ - x2;
-        y2 = y1_ - y2;
+        x2 = x1_prev - n * x2;
+        y2 = y1_prev - n * y2;
         p = x1 * a + y1 * b;
         q = x2 * a + y2 * b;
     }
@@ -31,7 +30,7 @@ fn egcd(a: i32, b:i32) -> (i32, i32) {
 
 fn main() {
     let a = 512;
-    let b = 64;
+    let b = 12;
     let e = gcd(a, b);
     let (x, y) = egcd(a, b);
     println!("{a}*{x} + {b}*{y} = {e}");
